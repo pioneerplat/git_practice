c37978b4aa58bff2370649575834cebcfd2b533a
pioneerplat <pioneerplat@gmail.com>
2022-04-06 00:36:27 +0900

    mama4

--- lib/main.dart
+++ lib/main.dart
@@ -22,10 +22,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
 
-  // mama
-  // master
-  // mama3
-
   final String title;
 
   @override
